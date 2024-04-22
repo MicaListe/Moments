12e06558f2898271d7b43ad13e08f53a41adcff6
santiagodietrich <santydietrich@gmail.com>
2024-04-22 11:49:57 -0300

    cargando lugares x3

--- Server/src/models/Lugar.js
+++ Server/src/models/Lugar.js
@@ -22,7 +22,7 @@ module.exports=(sequelize)=>{
                 allowNull: false
             },
             description:{
-                type: DataTypes.STRING,
+                type: DataTypes.TEXT,
                 allowNull: false
             }
         },
