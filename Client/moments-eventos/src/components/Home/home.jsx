f330f471b5a179b26b1c2ccadeff9a0910a73de6
santiagodietrich <santydietrich@gmail.com>
2024-05-02 18:54:41 -0300

    renegando con el carrusel y su descripcion

--- Client/moments-eventos/src/components/Home/home.jsx
+++ Client/moments-eventos/src/components/Home/home.jsx
@@ -1,34 +1,46 @@
-import playa from "../../assets/playa.jpg"
-import quinta from "../../assets/quinta.jpg"
-import salon from "../../assets/salon.jpg"
+import React from 'react';
+import playa from "../../assets/playa.png";
+import quinta from "../../assets/quinta.png";
+import salon from "../../assets/salon.png";
 
-export default function Home(){
-    return(
-        <div id="carouselExampleIndicators" class="carousel slide" dataRide="carousel">
-            <ol class="carousel-indicators">
-                <li dataTarget="#carouselExampleIndicators" dataSlideTo="0" class="active"></li>
-                <li dataTarget="#carouselExampleIndicators" dataSlideTo="1"></li>
-                <li dataTarget="#carouselExampleIndicators" dataSlideTo="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="d-block w-50" src={quinta} alt="First slide"/>
-                </div>
-                <div class="carousel-item">
-                <img class="d-block w-50" src={salon} alt="Second slide"/>
-                </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src={playa} alt="Third slide"/>
+export default function Home() {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <div className="w-25 p-2">
+                <h2>Eventos</h2>
+                <p>Experimenta la belleza incomparable de celebrar tu fiesta en los lugares más espectaculares y soñados.
+                    Sumérgete en una experiencia inolvidable que te transportará a un mundo de encanto y alegría.</p>
+            </div>
+            <div className="w-50 p-4">
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src={quinta} alt="First slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={salon} alt="Second slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={playa} alt="Third slide"/>
+                        </div>
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="" role="button" dataSlide="prev">
-                <span class="carousel-control-prev-icon" ariaHidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" ariaHidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
-    )
+    );
 }
+
+
