79c6fe9a55266d7aa61843c997674bed19f69435
MicaListe <micaealiste@gmail.com>
2024-05-08 20:29:41 -0300

    commit

--- Client/moments-eventos/src/App.jsx
+++ Client/moments-eventos/src/App.jsx
@@ -5,20 +5,11 @@ import CarouselPlaces  from './components/Carousel/carouselPlaces';
 import CarouselCatering from './components/Carousel/carouselCatering';
 import CarouselDecoration from './components/Carousel/carouselDecoration';
 import axios from "axios"
-import { useEffect} from 'react';
-import {useDispatch} from "react-redux"
-import { getEvents } from './Redux/actions';
 
 axios.defaults.baseURL="https://moments-self.vercel.app"
 
 function App() {
   
-  const dispatch= useDispatch()
-
-  useEffect(()=>{
-    dispatch(getEvents())
-  },[])
-
   return (
     <div className="App">
       <Navbar/>
