33dfc71d44d3b8e9255d2b3fc1a1b3de38f1d14c
MicaListe <micaealiste@gmail.com>
2024-05-08 18:36:09 -0300

    x

--- Server/src/server.js
+++ Server/src/server.js
@@ -18,18 +18,18 @@ server.use(cors({
    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Access-Control-Allow-Origin'], // Cabeceras permitidas
  }));
 
-server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Credentials', 'true');
-   res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-   );
-   res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE'
-   );
-   next();
-});
+// server.use((req, res, next) => {
+//    res.header('Access-Control-Allow-Origin', '*');
+//    res.header('Access-Control-Allow-Credentials', 'true');
+//    res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept'
+//    );
+//    res.header(
+//       'Access-Control-Allow-Methods',
+//       'GET, POST, OPTIONS, PUT, DELETE'
+//    );
+//    next();
+// });
 
 module.exports = server;
