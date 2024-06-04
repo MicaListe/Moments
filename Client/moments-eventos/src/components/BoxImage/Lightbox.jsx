0c71cf1deb981787b55694b1514edd18f9791815
MicaListe <micaealiste@gmail.com>
2024-06-04 19:57:08 -0300

    modal

---
+++ Client/moments-eventos/src/components/BoxImage/Lightbox.jsx
@@ -0,0 +1,21 @@
+import { Modal, Carousel } from 'react-bootstrap';
+
+export default function ModalImage({modal, image, name, description, handleClose}) {
+    return (
+        <Modal show={modal} onHide={handleClose}>
+            <Modal.Header closeButton> {/* Encabezado */}
+                <Modal.Title>{name}</Modal.Title> {/* Titulo */}
+            </Modal.Header>
+            <Modal.Body> {/* Cuerpo*/}
+                <Carousel>
+                    {image.map((img, index) => (
+                        <Carousel.Item key={index} style={{height:"300px"}}>
+                            <img src={img} className=" w-100" alt={`Imagen ${index + 1}`} style={{ objectFit: "cover", height:"300px"}}/>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                <p className='mt-4'>{description}</p>
+            </Modal.Body>
+        </Modal>
+    );
+}
