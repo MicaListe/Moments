372e2d62cdb1df692c8f6f154ff24a51b97221c7
MicaListe <micaealiste@gmail.com>
2024-07-25 19:18:16 -0300

    c

--- Client/moments-eventos/src/components/Calendario/Calendario.jsx
+++ Client/moments-eventos/src/components/Calendario/Calendario.jsx
@@ -1,37 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { InlineWidget } from 'react-calendly';
 
 const Calendario = () => {
-  useEffect(() => {
-    const initCalendly = () => {
-      if (window.Calendly) {
-        window.Calendly.initInlineWidget({
-          url: 'https://calendly.com/momentsevents18/reunion-eventos',
-          parentElement: document.querySelector('.calendly-inline-widget'),
-          prefill: {},
-          utm: {}
-        });
-      } else {
-        console.error('Calendly object is not available');
-      }
-    };
-
-    // Verifica si el script de Calendly está cargado y llama a la función de inicialización
-    const checkCalendlyScript = () => {
-      if (window.Calendly) {
-        initCalendly();
-      } else {
-        setTimeout(checkCalendlyScript, 100); // Reintenta después de 100ms
-      }
-    };
-
-    checkCalendlyScript(); // Inicia el proceso de verificación
-
-    // No es necesario limpiar nada aquí porque el script ya está en el HTML
-  }, []);
-
   return (
     <div className="container mt-5">
-      <div className="calendly-inline-widget" style={{ minWidth: '320px', height: '700px' }}></div>
+      <InlineWidget
+        url="https://calendly.com/momentsevents18/reunion-eventos"
+        styles={{ minWidth: '320px', height: '700px' }}
+      />
     </div>
   );
 };
