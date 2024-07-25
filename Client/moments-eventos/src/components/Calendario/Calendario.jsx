41664a7bee7a229f3bfea7a86320ee65f890e7dc
MicaListe <micaealiste@gmail.com>
2024-07-25 18:10:32 -0300

    c

--- Client/moments-eventos/src/components/Calendario/Calendario.jsx
+++ Client/moments-eventos/src/components/Calendario/Calendario.jsx
@@ -12,7 +12,7 @@ const Calendario = () => {
     script.onload = () => {
       if (window.Calendly) {
         window.Calendly.initInlineWidget({
-          url: 'https://calendly.com/momentsevents18/30min?primary_color=e8cd29',
+          url: 'https://calendly.com/momentsevents18/30min',
           parentElement: document.querySelector('.calendly-inline-widget'),
           prefill: {},
           utm: {}
