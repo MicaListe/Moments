ce415a217c2272ae936a8721050b68fc94b8c03c
MicaListe <micaealiste@gmail.com>
2024-07-25 19:52:29 -0300

    probando

--- Client/moments-eventos/src/components/Calendario/Calendario.jsx
+++ Client/moments-eventos/src/components/Calendario/Calendario.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react';
 import { InlineWidget } from 'react-calendly';
 
-
 const Calendario = () => {
-
 
   useEffect(() => {
     const handleEventScheduled = (e) => {
       if (e.data.event && e.data.event === 'calendly.event_scheduled') {
+        // Redirige a la URL externa
         window.location.href = 'https://moments-3oti.vercel.app/formulario';
       }
     };
 
+    // Escucha los eventos de Calendly
     window.addEventListener('message', handleEventScheduled);
 
     return () => {
-      document.body.removeChild(script);
+      // Limpia el listener al desmontar el componente
       window.removeEventListener('message', handleEventScheduled);
     };
   }, []);
