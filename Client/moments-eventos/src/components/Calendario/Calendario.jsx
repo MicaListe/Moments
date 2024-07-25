fbd348fcf3c14e80eb853f27390582a213302ea2
MicaListe <micaealiste@gmail.com>
2024-07-25 18:48:47 -0300

    c

--- Client/moments-eventos/src/components/Calendario/Calendario.jsx
+++ Client/moments-eventos/src/components/Calendario/Calendario.jsx
@@ -5,32 +5,29 @@ const Calendario = () => {
     const script = document.createElement('script');
     script.src = 'https://assets.calendly.com/assets/external/widget.js';
     script.async = true;
-    document.body.appendChild(script);
-
-    //Cuando se detecta el evento calendly.event_scheduled, se redirige al usuario a http://localhost:5173/formulario.
-    const handleEventScheduled = (e) => {
-      if (e.data.event && e.data.event === 'calendly.event_scheduled') {
-        window.location.href = 'https://moments-3oti.vercel.app/formulario';
+    script.onload = () => {
+      console.log("Calendly script loaded");
+      if (window.Calendly) {
+        window.Calendly.initInlineWidget({
+          url: 'https://calendly.com/momentsevents18/30min?primary_color=e8cd29',
+          parentElement: document.querySelector('.calendly-inline-widget'),
+          prefill: {},
+          utm: {}
+        });
+      } else {
+        console.error("Calendly object is not available");
       }
     };
-
-    window.addEventListener('message', handleEventScheduled);
-
-    //Inicializar el widget de Calendly una vez que el script de Calendly se haya cargado correctamente
-    script.onload = () => {
-      window.Calendly.initInlineWidget({
-        url: 'https://calendly.com/momentsevents18/reunion-eventos',
-        parentElement: document.querySelector('.calendly-inline-widget'),
-        prefill: {},
-        utm: {}
-      });
+    script.onerror = () => {
+      console.error("Error loading Calendly script");
     };
-    //limpieza que se ejecuta cuando el componente de React se desmonta.
+    document.body.appendChild(script);
+  
     return () => {
       document.body.removeChild(script);
-      window.removeEventListener('message', handleEventScheduled);
     };
   }, []);
+  
 
   return (
     <div className="container mt-5">
