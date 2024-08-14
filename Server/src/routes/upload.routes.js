ecaaf1568384229c7cae6b68ce649983c25a0f3d
santiagodietrich <santydietrich@gmail.com>
2024-08-14 17:11:24 -0300

    implementacion cloudinary

---
+++ Server/src/routes/upload.routes.js
@@ -0,0 +1,15 @@
+const {Router} = require("express");
+
+const router = Router();
+
+const Multer = require("multer");
+
+const UploadControllers = require("../controllers/upload");
+
+const storage = new Multer.memoryStorage();
+
+const upload = Multer({storage});
+
+router.post("/", upload.single("my_file"),  UploadControllers.uploadImage);
+
+module.exports = router;
